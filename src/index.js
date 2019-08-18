5f8e469900e9ec1ad035afbd75bed39933f3dacd
Andrzej Skrodki <andrzej.skrodzki@gmail.com>
2019-08-18 23:31:05 +0200

    Add workflows

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
     workflow.init();
 } else {
-    if (args[0] === "token") {
+    const command = args.shift();
+    if (command === "token") {
         workflow.set_token(args[1]);
-    } else if (args[0] === "workspace") {
+    } else if (command === "workspace") {
         workflow.get_workspaces();
-    } else if (args[0] === "project") {
-        // project(args[1])
+    } else if (command === "project") {
+        workflow.get_projects(args[1]);
+    } else if (command === "entry") {
+        workflow.save_time_entry(...args);
     } else {
         workflow.error("Unknown command");
     }
